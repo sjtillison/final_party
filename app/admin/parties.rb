fdc21fc5e63cda46db06fe7798dab2787fbc889d
Raghu Betina <raghu@firstdraft.com>
2018-03-13 15:11:26 +0000

    Whitelist columns in Party for ActiveAdmin

--- app/admin/parties.rb
+++ app/admin/parties.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Party do
+
+ permit_params :name, :type_id, :city_id, :start_date, :end_date, :lodging_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
