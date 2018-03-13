e94e072827d42e66ad216c351923b2445e12f31c
Raghu Betina <raghu@firstdraft.com>
2018-03-13 15:11:32 +0000

    Whitelist columns in Type for ActiveAdmin

--- app/admin/types.rb
+++ app/admin/types.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Type do
+
+ permit_params :event_type
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
