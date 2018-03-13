c9958f813abf8794ed8b9cb976c8231b3e3605e5
Raghu Betina <raghu@firstdraft.com>
2018-03-13 15:11:44 +0000

    Whitelist columns in Attendee for ActiveAdmin

--- app/admin/attendees.rb
+++ app/admin/attendees.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Attendee do
+
+ permit_params :user_id, :party_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
