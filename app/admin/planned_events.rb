e9cf558641558f223bc0166c02bfabbf0c94b733
Raghu Betina <raghu@firstdraft.com>
2018-03-13 15:11:38 +0000

    Whitelist columns in PlannedEvent for ActiveAdmin

--- app/admin/planned_events.rb
+++ app/admin/planned_events.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register PlannedEvent do
+
+ permit_params :party_id, :activity_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
