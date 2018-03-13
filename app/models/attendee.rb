aae0146d1678b66d98d011553cd5dfd20cc91c8d
Raghu Betina <raghu@firstdraft.com>
2018-03-13 15:11:11 +0000

    Direct association between users and attendees

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,6 +1,8 @@
 class Attendee < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   belongs_to :party
 
   # Indirect associations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :attendees,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
